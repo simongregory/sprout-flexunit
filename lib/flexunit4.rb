a0f4297f89b4d7c320dca189c5ccc9cf18d4c3e5
Simon Gregory <simon@helvector.org>
2011-05-08 20:39:22 +0100

    • Removing comment.

--- lib/flexunit4.rb
+++ lib/flexunit4.rb
@@ -20,9 +20,8 @@ Sprout::Specification.new do |s|
     t.url          = "http://flexunit.org/releases/flexunit-4.1.0-8-4.1.0.16076.zip"
     t.md5          = "11b355d269be39c03e24aa1143ef4c1c"
 
-    # FlexUnit SWCs:
-    t.add_library :flexunit_flex, "flexunit/flexunit-4.1.0-8-flex_4.1.0.16076.swc"
     t.add_library :flexunit_as3, "flexunit/flexunit-4.1.0-8-as3_4.1.0.16076.swc"
+    t.add_library :flexunit_flex, "flexunit/flexunit-4.1.0-8-flex_4.1.0.16076.swc"
     t.add_library :flexunit_aircilistener, "flexunit/flexunit-aircilistener-4.1.0-8-4.1.0.16076.swc"
     t.add_library :flexunit_cilistener, "flexunit/flexunit-cilistener-4.1.0-8-4.1.0.16076.swc"
     t.add_library :flexunit_flexcoverlistener, "flexunit/flexunit-flexcoverlistener-4.1.0-8-4.1.0.16076.swc"
@@ -30,4 +29,3 @@ Sprout::Specification.new do |s|
     t.add_library :flexunit_extensions, "flexunit/fluint-extensions-4.1.0-8-4.1.0.16076.swc"
   end
 end
-
